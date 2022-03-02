f8ca02ef1934bb82c39b93b4f33992b2e7adc098
ilijaradojkovic <radojkovicika@gmail.com>
2022-03-02 11:03:10 +0100

    Dodate toString,equals i hashCode metode klase Autor i Knjiga

--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -17,11 +17,47 @@ public class Knjiga {
 		this.isbn = isbn;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((autori == null) ? 0 : autori.hashCode());
+		result = prime * result + (int) (isbn ^ (isbn >>> 32));
+		result = prime * result + izdanje;
+		result = prime * result + ((izdavac == null) ? 0 : izdavac.hashCode());
+		result = prime * result + ((naslov == null) ? 0 : naslov.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		if (autori == null) {
+			if (other.autori != null)
+				return false;
+		} else if (!autori.equals(other.autori))
+			return false;
+		if (isbn != other.isbn)
+			return false;
+	
+		
+		return true;
+	}
 	public String getIzdavac() {
 		return izdavac;
 	}
 	public void setIzdavac(String izdavac) {
 		this.izdavac = izdavac;
+	}
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", autori=" + autori + ", izdavac=" + izdavac
+				+ ", izdanje=" + izdanje + "]";
 	}
 	public int getIzdanje() {
 		return izdanje;
